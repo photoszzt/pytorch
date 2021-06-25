ecdaba16caba4646d1d33abddab90839a98ef4cf
Zafar <cc.rafaz@zafar.cc>
2021-06-25 00:11:58 -0700

    [sparsity] Sparsifier class
    ghstack-source-id: 14ccb1ad19b52bf8b6616ba98b29e32e286bfd89
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/58704

--- torch/ao/sparsity/__init__.py
+++ torch/ao/sparsity/__init__.py
@@ -1,7 +1,18 @@
+from . import sparsifier
+
+# Sparsifier
+from .sparsifier import BaseSparsifier
+
 # Parametrizations
 from .parametrization import MulBy
 
+# Variables
+from ._variables import get_sparse_mapping
+from ._variables import get_dynamic_sparse_quantized_mapping
+from ._variables import get_static_sparse_quantized_mapping
+
 # === Experimental ===
+
 # Parametrizations
 from .experimental.pruner.parametrization import PruningParametrization
 
