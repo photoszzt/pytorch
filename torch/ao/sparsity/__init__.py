c04588c149567baa8fc498e12d2ecf0af4e21053
Zafar <cc.rafaz@zafar.cc>
2021-06-25 01:01:31 -0700

    [sparsity] WeightNormSparsifier
    ghstack-source-id: 5b7f2cb447f3336231427cc2e275c5eb0f08a7c0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/58955

--- torch/ao/sparsity/__init__.py
+++ torch/ao/sparsity/__init__.py
@@ -1,15 +1,14 @@
-from . import sparsifier
-
-# Sparsifier
-from .sparsifier import BaseSparsifier
-
-# Parametrizations
-from .parametrization import MulBy
-
 # Variables
 from ._variables import get_sparse_mapping
 from ._variables import get_dynamic_sparse_quantized_mapping
 from ._variables import get_static_sparse_quantized_mapping
+
+# Sparsifier
+from .sparsifier.base_sparsifier import BaseSparsifier
+from .sparsifier.weight_norm_sparsifier import WeightNormSparsifier
+
+# Parametrizations
+from .sparsifier.parametrization import MulBy
 
 # === Experimental ===
 
@@ -18,3 +17,4 @@ from .experimental.pruner.parametrization import PruningParametrization
 
 # Pruner
 from .experimental.pruner.base_pruner import BasePruner
+
